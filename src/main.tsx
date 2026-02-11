de77a4c728cd5b5c54bd19e7fcca32254e051543
muthuannamalai12 <muthuannamalai2002@gmail.com>
2026-02-11 12:20:16 +0530

    Added entity GUID to logs to make it visible in the logs UI

--- src/main.tsx
+++ src/main.tsx
@@ -37,25 +37,26 @@ const options = {
 // Set the userId for the browser agent
 // browserAgent.setUserId('user-1234-v1.0')
 
-// const row = {
-//   jobId: 'JOB-777-ABC',
-//   id: 'QUOTE-999-XYZ'
-// }
+const row = {
+  jobId: 'JOB-777-ABC',
+  id: 'QUOTE-999-XYZ'
+}
 
-// if (typeof newrelic !== 'undefined' && newrelic.log) {
-//     console.log('QuotesTable: newrelic.log is valid'); 
+if (typeof newrelic !== 'undefined' && newrelic.log) {
     
-//     newrelic.log(
-//         'DEV, QuotesTable, onButtonClick, Browser Agent v1.308 npm package approach',
-//         {
-//             // This "tricks" TypeScript into allowing lowercase
-//             level: 'warn' as any,
-//             customAttributes: {
-//                 entityId: `jobID: ${row.jobId}, id: ${row.id}`,
-//             },
-//         }
-//     );
-// }
+    newrelic.log(
+        'DEV, QuotesTable, onButtonClick, Browser Agent v1.308 npm package approach',
+        {
+            level: 'WARN',
+            customAttributes: {
+                'entity.guid': 'MXxCUk9XU0VSfEFQUExJQ0FUSU9OfDYwMTUzOTI5MA',
+                entityId: `jobID: ${row.jobId}, id: ${row.id}`,
+                jobId: row.jobId,
+                quoteId: row.id
+            },
+        }
+    );
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
 
